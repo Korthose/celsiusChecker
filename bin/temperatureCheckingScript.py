9da5a6580b0ff511763a887350558c3733088619
Leo Korthaus <leo.korthaus@shapefruit.de>
2024-12-17 11:33:49 +0100

    time values should be more in sync now

--- bin/temperatureCheckingScript.py
+++ bin/temperatureCheckingScript.py
@@ -32,7 +32,7 @@ while True:
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
-        time.sleep(2.0)
+        time.sleep(0.1)
         continue
     except Exception as error:
         sensor.exit()
