6f9e9dea44a04f21407b7856a0198f9c3bedde27
Leo Korthaus <leo.korthaus@shapefruit.de>
2024-12-17 16:40:43 +0100

    Added php files to separate general HTML and PHP

--- Public/Partials/temperatureCard.php
+++ Public/Partials/temperatureCard.php
@@ -1,24 +1,138 @@
-<?php ?>
-<!--<div class="d-flex row justify-content-center">-->
-    <div class="card" style="width: 20rem;height: 25rem">
+<?php if ($currentTemperature ?? false == true) { ?>
+
+<div class="d-flex justify-content-center">
+    <div class="card">
         <div class="card-body">
             <!--Celsius-->
             <div class="celsius">
-                17c
+                <?= $temperature['celsius'] ?>°C
             </div>
             <div class="miscellaneous">
                 <div class="row">
                     <div class="fahrenheit col-6">
-                        70f
+                        <?= $temperature['fahrenheit'] ?>°F
                     </div>
                     <div class="humidity col-6">
-                        80%
+                        <?= $temperature['humidity'] ?>%
                     </div>
-                    <div class="timer col-12">
-                        0:04
+                    <div class="timer col-12" >
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
-<!--</div>-->
+</div>
+
+<?php } elseif ($temperatureSummary ?? false == true) {?>
+
+    <div class="summary d-flex row justify-content-center">
+        <div class="col-12  col-md-custom">
+            <div class="card">
+                <div class="card-body">
+                    <!--Celsius-->
+                    <div class="celsius">
+                        <?= $temperature['celsius'] ?>°C
+                    </div>
+                    <div class="miscellaneous">
+                        <div class="row">
+                            <div class="fahrenheit col-6">
+                                <?= $temperature['fahrenheit'] ?>°F
+                            </div>
+                            <div class="humidity col-6">
+                                <?= $temperature['humidity'] ?>%
+                            </div>
+                            <div class="hot col-6">
+                                <?= $temperature['celsius'] ?>°C/<?= $temperature['fahrenheit'] ?>°F
+                            </div>
+                            <div class="cold col-6">
+                                <?= $temperature['celsius'] ?>°C/<?= $temperature['fahrenheit'] ?>°F
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12  col-md-custom">
+            <div class="card">
+                <div class="card-body">
+                    <!--Celsius-->
+                    <div class="celsius">
+                        <?= $temperature['celsius'] ?>°C
+                    </div>
+                    <div class="miscellaneous">
+                        <div class="row">
+                            <div class="fahrenheit col-6">
+                                <?= $temperature['fahrenheit'] ?>°F
+                            </div>
+                            <div class="humidity col-6">
+                                <?= $temperature['humidity'] ?>%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12  col-md-custom">
+            <div class="card">
+                <div class="card-body">
+                    <!--Celsius-->
+                    <div class="celsius">
+                        <?= $temperature['celsius'] ?>°C
+                    </div>
+                    <div class="miscellaneous">
+                        <div class="row">
+                            <div class="fahrenheit col-6">
+                                <?= $temperature['fahrenheit'] ?>°F
+                            </div>
+                            <div class="humidity col-6">
+                                <?= $temperature['humidity'] ?>%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12  col-md-custom">
+            <div class="card">
+                <div class="card-body">
+                    <!--Celsius-->
+                    <div class="celsius">
+                        <?= $temperature['celsius'] ?>°C
+                    </div>
+                    <div class="miscellaneous">
+                        <div class="row">
+                            <div class="fahrenheit col-6">
+                                <?= $temperature['fahrenheit'] ?>°F
+                            </div>
+                            <div class="humidity col-6">
+                                <?= $temperature['humidity'] ?>%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12  col-md-custom">
+            <div class="card">
+                <div class="card-body">
+                    <!--Celsius-->
+                    <div class="celsius">
+                        <?= $temperature['celsius'] ?>°C
+                    </div>
+                    <div class="miscellaneous">
+                        <div class="row">
+                            <div class="fahrenheit col-6">
+                                <?= $temperature['fahrenheit'] ?>°F
+                            </div>
+                            <div class="humidity col-6">
+                                <?= $temperature['humidity'] ?>%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php } ?>
--- Public/Views/summary.php
+++ Public/Views/summary.php
@@ -1,12 +1,17 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../../Private/Model/Temperature.php';
 require_once __DIR__ . '/../../bin/src/functions.inc.php';
 
-$temperature = \Model\temperature::newestTemperature();
-$title = 'Current temperature';
+
+$temperature = \Model\temperature::last5Days(3);
+$title = 'Last 5 Days';
 
 print_r($temperature);
 
-$currentTemperature = true;
+$temperatureSummary = true;
 require_once __DIR__ . '/../template.php';
 
