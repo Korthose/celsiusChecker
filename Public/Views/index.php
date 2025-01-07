9e70ad62aa34d22ed74718f14fe9bf36a30a6f41
Leo Korthaus <leo.korthaus@shapefruit.de>
2025-01-07 15:02:43 +0100

    Removed old debugging code

--- Public/Views/index.php
+++ Public/Views/index.php
@@ -5,8 +5,6 @@ require_once __DIR__ . '/../../bin/src/functions.inc.php';
 $temperature = \Model\temperature::newestTemperature();
 $title = 'Current temperature';
 
-print_r($temperature);
-
 $currentTemperature = true;
 require_once __DIR__ . '/../template.php';
 
--- Public/Views/summary.php
+++ Public/Views/summary.php
@@ -1,16 +1,11 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 require_once __DIR__ . '/../../Private/Model/Temperature.php';
 require_once __DIR__ . '/../../bin/src/functions.inc.php';
 
 
-$temperature = \Model\temperature::last5Days(3);
+$temperature = \Model\temperature::last5Days(5);
 $title = 'Last 5 Days';
-
-print_r($temperature);
 
 $temperatureSummary = true;
 require_once __DIR__ . '/../template.php';
