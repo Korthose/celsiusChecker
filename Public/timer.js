06edf7e03f4b7d691000657e3e53d1fd357506c5
Leo Korthaus <leo.korthaus@shapefruit.de>
2025-01-08 18:41:12 +0100

    Fixed value error for timer

--- Public/timer.js
+++ Public/timer.js
@@ -1,5 +1,5 @@
 document.querySelector('.timer').innerHTML = '1:00';
-var seconds_left = 600;
+var seconds_left = 60   ;
 var interval = setInterval(function () {
     seconds_left--;
     document.querySelector('.timer').innerHTML = '0:' + seconds_left.toString().padStart(2, '0');
