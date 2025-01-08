13229952385b98bf59c0b52fe43341227a75641e
Leo Korthaus <leo.korthaus@shapefruit.de>
2025-01-08 18:34:58 +0100

    Updated timer.js

--- Public/timer.js
+++ Public/timer.js
@@ -1,10 +1,10 @@
-    var seconds_left = 60;
-    var interval = setInterval(function () {
-        document.querySelector('.timer').innerHTML = --seconds_left;
-
-        if (seconds_left <= 0) {
-            document.querySelector('.timer').innerHTML = "0:00";
-            location.reload();
-            clearInterval(interval);
-        }
-    }, 1000);
+document.querySelector('.timer').innerHTML = '1:00';
+var seconds_left = 600;
+var interval = setInterval(function () {
+    seconds_left--;
+    document.querySelector('.timer').innerHTML = '0:' + seconds_left.toString().padStart(2, '0');
+    if (seconds_left == 0) {
+        document.querySelector('.timer').innerHTML = '';
+        location.reload();
+    }
+}, 1000);
